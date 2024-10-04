662ad9364d0db4983019162dd8bccc9e62688e58
‘patrick’ <patrickserrats888@gmail.com>
2024-10-04 17:22:31 +0200

    Main Page + Song form

---
+++ mvc/form.php
@@ -0,0 +1,31 @@
+<pre>
+<?php
+
+print_r($_POST);
+
+print_r($_FILES);
+
+ print_r(getcwd());
+
+$musicFilePath = "../music/" . $_FILES["music"]["name"];
+$imageFilePath = "../img/" . $_FILES["img"]["name"];
+$gameFilePath = "../game/" . $_FILES["game"]["name"];
+
+move_uploaded_file($_FILES["music"]["tmp_name"], "../music/" . $_FILES["music"]["name"]);
+move_uploaded_file($_FILES["img"]["tmp_name"], "../img/" . $_FILES["img"]["name"]);
+move_uploaded_file($_FILES["game"]["tmp_name"], "../game/" . $_FILES["game"]["name"]);
+
+$data = array(
+    "title" => $_POST["title"],
+    "artist" => $_POST["artist"],
+    "music" => $musicFilePath,
+    "game" => $gameFilePath,
+    "img" => $imageFilePath
+);
+
+$jsonData = json_decode(file_get_contents("playlist.json"), true);
+$jsonData["songs"][] = $data;
+file_put_contents("playlist.json", json_encode($jsonData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+
+?>
+</pre>
---
+++ mvc/songform.php
@@ -0,0 +1,84 @@
+<?php
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+include "config.php";
+$error = $_GET["error"];
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Exemple de formulari</title>
+  </head>
+  <body class="d-flex flex-column min-vh-100" style="background-image: url('/img/bg.png'); background-size: cover; background-position: center; background-repeat: no-repeat;">  
+  
+  <nav class="navbar navbar-dark justify-content-md-center" style="background-color: #E6E6E6; border: 3px solid black; padding: 20px 0;">
+    <div class="d-flex">
+    <a href="/index.html" class="navbar-brand mb-0 h1" style="color: black; border-right: 2px solid black; padding-right: 10px; margin-right: 10px;">Pagina Principal</a>
+    <a href="mvc/songform.php" class="navbar-brand mb-0 h1" style="color: black; border-right: 2px solid black; padding-right: 10px; margin-right: 10px;">Añadir Cancion</a>
+    <a href="mvc/index.php" class="navbar-brand mb-0 h1" style="color: black; border-right: 2px solid black; padding-right: 10px; margin-right: 10px;">Jugar</a>
+    <a href="mvc/index.php" class="navbar-brand mb-0 h1" style="color: black;">Clasificaciones</a>
+    </div>
+  </nav>
+
+  <div class="container mt-5 flex-grow-1">
+    <div class="row justify-content-center">
+      <div class="col-10 text-center p-5" style="border: 2px solid black; background-color: #e6e6e6c2; border-radius: 10px;">
+        <h1 class="display-4 mb-4">Añadir Cancion</h1>
+          <?php if (isset($error)) {
+                ?>
+          <div class="alert alert-danger" role="alert">
+            Error!!! Has d'omplir els camps.
+          </div>
+                <?php
+          }?>
+          <form action="form.php" enctype="multipart/form-data" method="post">
+    <div class="form-group">
+        <label for="inputtitol">Titol</label>
+        <input name="title" type="text" class="form-control" id="inputtitol" placeholder="El nom de la canço">
+    </div>
+    <div class="form-group">
+        <label for="inputartista">Artista</label>
+        <input name="artist" type="text" class="form-control" id="inputartista" placeholder="El nom de artista">
+    </div>
+    <div class="form-group">
+        <label for="inputmusica">Musica</label>
+        <input name="music" type="file" class="form-control" id="inputmusica" placeholder="El fitxer de musica">
+    </div>
+    <div class="form-group">
+        <label for="inputimage">Portada</label>
+        <input name="img" type="file" class="form-control" id="inputimage" placeholder="La portada">
+    </div>
+    <div class="form-group">
+        <label for="inputgame">Joc</label>
+        <input name="game" type="file" class="form-control" id="inputgame" placeholder="El fitxer de joc">
+    </div>
+    <button type="submit" class="btn btn-primary">Enviar</button>
+          </form>
+
+      </div>
+    </div>
+  </div>
+
+  <footer class="p-3 text-center" style="border: 2px solid black; background-color: rgba(255, 255, 255, 0);">
+    <p class="mb-0" style="color: rgb(255, 255, 255);"> 
+      correu@gmail.com
+      Instagram
+      Facebook
+      Youtube</p>
+  </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
